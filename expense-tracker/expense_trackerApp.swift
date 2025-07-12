146baa089378eb7c50306add2212527a4118c9ab
PrajjvalMehra <mehraprajjval@gmail.com>
2025-07-12 09:18:17 -0500

    Initial Commit

---
+++ expense-tracker/expense_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expense_trackerApp.swift
+//  expense-tracker
+//
+//  Created by Prajjval mehra on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct expense_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ expense-trackerTests/expense_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  expense_trackerTests.swift
+//  expense-trackerTests
+//
+//  Created by Prajjval mehra on 7/12/25.
+//
+
+import Testing
+@testable import expense_tracker
+
+struct expense_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
